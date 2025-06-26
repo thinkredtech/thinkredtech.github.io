e7b8dd20e003deb57e810e582935bcf99e66513d
sayak-sarkar <sayak-sarkar@users.noreply.github.com>
2025-06-26 08:50:34 +0000

    deploy: 38e7dd2f54600b15128182f9d04954c82102a9fd

--- assets/iconUtils-C8rXBQXA.js
+++
@@ -1,2 +0,0 @@
-import{R as t}from"./react-core-BlqnBwT7.js";const o=(e,r)=>t.createElement(e,r);export{o as r};
-//# sourceMappingURL=iconUtils-C8rXBQXA.js.map
---
+++ assets/iconUtils-CiAVxo9O.js
@@ -0,0 +1,2 @@
+import{R as n}from"./react-core-BlqnBwT7.js";const c=(e,r)=>{const t=e;return n.createElement(t,r)};export{c as r};
+//# sourceMappingURL=iconUtils-CiAVxo9O.js.map
